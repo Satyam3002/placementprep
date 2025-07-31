bfe85417764e6bbccc774f825040dace7bb9f9b8
Satyam Laheri <satyamlaheri3@gmail.com>
2025-07-31 16:21:27 +0530

    BST

---
+++ BST/bst.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->data = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Function prototype
+Node *insertIntoBST(Node *root, int data);
+
+void CreateBst(Node *&root)
+{
+    cout << "Enter the value of node (-1 to stop): ";
+    int data;
+    cin >> data;
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cout << "Enter the value of node (-1 to stop): ";
+        cin >> data;
+    }
+}
+
+Node *insertIntoBST(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return new Node(data);
+    }
+
+    if (data > root->data)
+    {
+        root->right = insertIntoBST(root->right, data);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, data);
+    }
+
+    return root;
+}
+
+void levelOrderTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = NULL;
+    CreateBst(root);
+
+    cout << "\nLevel Order Traversal:\n";
+    levelOrderTraversal(root);
+
+    cout << "\nInorder: ";
+    inorder(root);
+    cout << "\nPreorder: ";
+    preorder(root);
+    cout << "\nPostorder: ";
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
