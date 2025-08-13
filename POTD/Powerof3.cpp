a7245ab2302b1974e2d8b2c000cfad1901b3079d
Satyam Laheri <satyamlaheri3@gmail.com>
2025-08-13 23:56:43 +0530

    13

---
+++ POTD/Powerof3.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+
+        long long ans = 1;
+        while (ans <= n)
+        {
+            if (ans == n)
+                return true;
+            ans *= 3;
+        }
+
+        return false;
+    }
+};
