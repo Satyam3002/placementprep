17313b35e4c546e23bf2efe2a49f8da5c4995054
Satyam Laheri <satyamlaheri3@gmail.com>
2025-08-22 23:34:18 +0530

    Create MinimumAreaToCoverAllones.cpp

---
+++ POTD/MinimumAreaToCoverAllones.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int minimumArea(vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int maxRow = -1;
+        int minRow = m;
+        int maxCol = -1;
+        int minCol = n;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    minRow = min(minRow, i);
+                    maxRow = max(maxRow, i);
+
+                    minCol = min(minCol, j);
+                    maxCol = max(maxCol, j);
+                }
+            }
+        }
+
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+};
